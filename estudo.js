c5e80d52a279d9227e0682db9f8765ffb4b808bf
beatrizbueno1 <108940046+beatrizbueno1@users.noreply.github.com>
2022-09-30 11:08:39 -0300

    Add files via upload

---
+++ estudo.js
@@ -0,0 +1,18 @@
+let tabuada = 2;
+let nome = "Beatriz Bueno";
+
+function escreva(){
+
+    document.write(tabuada +" x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*2)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*3)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*4)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*5)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*6)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*7)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*8)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*9)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*10)+"<br>");
+    document.write("Feito por" + nome);
+}
+
